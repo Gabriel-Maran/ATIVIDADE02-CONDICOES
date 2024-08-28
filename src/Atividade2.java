f42c58b57be9ebe46c92e688b40c0e03b6d478f4
Gabriel-Maran <gabriel.enzo.libero.maran@gmail.com>
2024-08-28 20:19:42 -0300

    Atividade1 pronta

---
+++ src/Atividade2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Atividade2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double nota = 0;
+        System.out.println("Informe a nota do Aluno(0-10): ");
+        nota = sc.nextDouble();
+        if(nota>10 || nota<0){
+            System.out.println("Digitou algo errado, tente novamente");
+        }else if(nota > 9) {
+            System.out.println("Nerd demais");
+        }else if (nota >= 9){
+            System.out.println("Excelente");
+        }else if (nota >= 7){
+            System.out.println("Bom");
+        }else if (nota >= 5){
+            System.out.println("Satisfatório");
+        }else if(nota < 5 && nota>=0){
+            System.out.println("Insatisfatório");
+        }
+    }
+}
